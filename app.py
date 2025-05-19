6dbc25c829ebb223229367bfd3010e28980314a2
OraMead <oramead@outlook.com>
2025-05-20 10:02:44 +1200

    Created admin.html
    Created admin page where admin can see all notes as reader. Only lets users remove tags if they are owner or have manage permission. Only lets owner add tags, delete, and access note options. Made code for including modals also based on same logic buttons use to save space on page. Created student_teacher table

--- app.py
+++ app.py
@@ -187,7 +187,7 @@ def process_note(note) -> dict:
 
     if len(note) > 6:
         note_data['owner'] = note[6]
-        note_data['permission'] = note[7]
+        note_data['permission'] = note[7] if len(note) > 7 and note[7] else 0    
     
     return note_data
 
@@ -251,7 +251,40 @@ def students():
 
 @app.route('/admin')
 def admin():
-    return render_template('admin.html', title='Admin', logged_in=is_logged_in())
+
+    if not is_logged_in() or session['role'] != 2:
+        return redirect(url_for('index'))
+    
+    note_list = fetch('''SELECT 
+                            n.note_id,
+                            n.title,
+                            s.name,
+                            n.fk_subject_id,
+                            (
+                                SELECT GROUP_CONCAT(t.tag_id || ':' || t.name, '|')
+                                FROM note_tag nt
+                                JOIN tag t ON nt.fk_tag_id = t.tag_id
+                                WHERE nt.fk_note_id = n.note_id
+                            ) AS tags,
+                            (
+                                SELECT GROUP_CONCAT(u.user_id || ':' || u.fname || ' ' || u.lname || ':' || sn2.permission, '|')
+                                FROM shared_note sn2
+                                JOIN user u ON sn2.fk_user_id = u.user_id
+                                WHERE sn2.fk_note_id = n.note_id
+                            ) AS shared,
+                            u.fname || ' ' || u.lname AS owner
+                        FROM note n
+                        JOIN subject s ON n.fk_subject_id = s.subject_id
+                        JOIN user u ON n.fk_user_id = u.user_id
+                        ORDER BY n.updated_at DESC;''')
+    
+    for i, note in enumerate(note_list):
+        note_list[i] = process_note(note)
+
+    return render_template('admin.html', 
+                           title='Admin', 
+                           logged_in=is_logged_in(),
+                           notes=note_list)
 
 
 @app.route('/more/<int:shared>')
